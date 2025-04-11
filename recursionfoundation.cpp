4c13f5fa10f56d29c6f6e54561eb0dfa9d1a6da3
Priyanshu <priyanshu22223@gmail.com>
2025-04-11 18:18:46 +0530

    Added new files

---
+++ recursionfoundation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+const int MOD=1e9+7;
+const int INF=LLONG_MAX>>1;
+int fact(int x){
+    if(x==0){
+        return 1;
+    }
+    return x*fact(x-1);
+}
+void solve(){
+cout<<fact(3);
+}
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+    int t;
+    //t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
